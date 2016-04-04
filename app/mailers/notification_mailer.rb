d61ceb12fbcd9d88e3b0b11f91b93ecf7e34016f
Ivan Heneghan <ivanheneghan@gmail.com>
2016-04-04 11:47:39 +0000

    43/44 - Email trigger

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,8 +2,9 @@ class NotificationMailer < ActionMailer::Base
   default from: "no-reply@nomsterapp.com"
 
   def comment_added(comment)
-    @place = comment.place
-    @place_owner = @place.user
-    mail(to: @place_owner.email, subject: "A comment has been added to #{@place.name}")
+    #@place = comment.place
+    #@place_owner = @place.user
+    #mail(to: @place_owner.email, subject: "A comment has been added to #{@place.name}")
+    mail(to: "ivanheneghan@gmail.com", subject: "A comment has been added to your place")
   end
 end
